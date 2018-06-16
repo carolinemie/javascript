db7684958e4fec4377a3566a5d63a93ecd7eeb01
Caroline Mie Chen <carolinemie@hotmail.com>
2018-06-16 11:25:16 -0300

    Initial commit

---
+++ exercicio.js
@@ -0,0 +1,42 @@
+let idade= prompt("Qual a sua idade?")
+let emprestimo= prompt("De quanto você precisa?")
+let salario= prompt("Quanto você ganha?")
+
+
+
+if (idade > 21 && emprestimo <= salario * 15 || emprestimo >= 1000 ) {
+    alert ("APROVADO")
+}
+
+else{
+    alert ("REPROVADO")
+} 
+
+/////////////////////////////////////////////////////////////////////////////
+
+/* EXERCICIO 02 - Tá tudo errado!!!! */
+
+let lista.lenght = 50
+
+let numero = prompt("Em que número estou pensando?")
+
+while (numero = true){
+    alert ("ACERTOU")
+    if (numero <  ){
+        alert
+    }
+}
+
+/* EXERCÍCIO 05 */
+
+let nome = prompt("Qual o seu nome?")
+let nascimento = prompt("Quando você nasceu?")
+let login = prompt("Cadastrar login")
+let senha = prompt("Digite sua senha")
+let premium = 
+
+let usuario = { nome:"caroline", login:"carol", senha:"dormir", nascimento:"10/07/1994", premium:true}
+
+
+
+
